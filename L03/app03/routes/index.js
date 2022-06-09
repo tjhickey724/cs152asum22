2898efbb1c85e5e67608c01ba42249eacc0e9190
tim <tjhickey@brandeis.edu>
2022-06-09 11:51:43 -0400

    added bio page

--- L03/app03/routes/index.js
+++ L03/app03/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use((req,res,next) => {
+   console.log("in the index router")
+   next()
+})
+
+router.get('/bio',
+  (req,res,next) => {
+   console.log("in the index router")
+   next()
+  },
+  (req,res,next) => {
+    res.render('bio')
+  }
+)
+
 module.exports = router;
