8866861b0880074432411402072a55766cdd3c0b
tim <tjhickey@brandeis.edu>
2022-06-09 12:20:22 -0400

    added layout code

--- L03/app03/app.js
+++ L03/app03/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const layouts = require("express-ejs-layouts");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(layouts)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
